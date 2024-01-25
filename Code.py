f748281a44ce5c0107fee6904a8b82fb1293f837
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2024-01-25 10:09:35 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Read data from CSV into Python
+mpg_from_csv = pd.read_csv("data.csv")
+
+# Set seaborn theme
+sns.set_theme(style="white", color_codes=True)
+
+# Use JointGrid directly to draw a custom plot
+g = sns.JointGrid(data=mpg_from_csv, x="Height", y="Leaf number", space=0, ratio=17)
+g.plot_joint(sns.scatterplot, size=mpg_from_csv["Yield"], sizes=(30, 120),
+             color="b", alpha=.6, legend=False)
+g.plot_marginals(sns.rugplot, height=1, color="g", alpha=.6)
+
+plt.show()
